c400ed5251da980275fd4e5764c9fc1488735ba5
Johnny Peterson <johnny.peterson@wgu.edu>
2020-11-10 11:24:23 -0700

    sample UITesting structure
    small project to show that multiple UItest can exist under one target with setup functions for each class.

--- TestUITestsUITests/TestTwo.swift
+++ TestUITestsUITests/TestTwo.swift
@@ -1,5 +1,5 @@
 //
-//  TestUITestsUITests.swift
+//  TestTwo.swift
 //  TestUITestsUITests
 //
 //  Created by Johnny Peterson on 11/10/20.
@@ -7,14 +7,17 @@
 
 import XCTest
 
-class TestUITestsUITests: XCTestCase {
+class TestTwo: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        print("this also worked")
+        print("This worked")
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -24,20 +27,8 @@ class TestUITestsUITests: XCTestCase {
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }

00f24fa0f5e99306074c28eaa88859826a095ae9
Johnny Peterson <johnny.peterson@wgu.edu>
2020-11-10 11:17:55 -0700

    Initial Commit

---
+++ TestUITests/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestUITests
+//
+//  Created by Johnny Peterson on 11/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestUITests/TestUITestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUITestsApp.swift
+//  TestUITests
+//
+//  Created by Johnny Peterson on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TestUITestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
